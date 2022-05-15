4b3de7764c5c70a3e243f40a4b6bcc07d666bca3
rolandovillca <rolandovillca@gmail.com>
2022-05-15 16:20:14 -0400

    updating lab14 mnv

--- lab14_mvc/w3d6/q1/index.js
+++ lab14_mvc/w3d6/q1/index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.set("port", process.env.PORT || 3000);
+const PORT = app.get("port");
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "view"));
+app.use("/views", express.static(path.join(__dirname, "view")));
 
 app.get("/", (req, res) => {
     const date = new Date();
@@ -12,9 +16,9 @@ app.get("/", (req, res) => {
     });
 });
 
-app.listen(3000, (err)=>{
+app.listen(PORT, (err)=>{
     if (err) {
         console.log(err);
     }
-    console.log("Server listening on port", 3000);
+    console.log("Server listening on port", PORT);
 });
